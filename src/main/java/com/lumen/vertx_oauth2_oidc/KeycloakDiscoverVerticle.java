9ac40f13e4a659a6191239b98aec05a2ea73a807
swellergis <shaun.weller@gmail.com>
2024-08-28 09:25:57 -0600

    code format changes

--- src/main/java/com/lumen/vertx_oauth2_oidc/KeycloakDiscoverVerticle.java
+++ src/main/java/com/lumen/vertx_oauth2_oidc/KeycloakDiscoverVerticle.java
@@ -14,15 +14,15 @@ import io.vertx.ext.web.Router;
 
 public class KeycloakDiscoverVerticle extends AbstractVerticle 
 {
-  private final static Logger LOG = Logger.getLogger(MainVerticle.class.getName());
+    private final static Logger LOG = Logger.getLogger(KeycloakDiscoverVerticle.class.getName());
 
-  private String bindAddress;
-  private int bindPort;
+    private String bindAddress;
+    private int bindPort;
 
-  private static final String BASE_URL = System.getenv("KEYCLOAK_BASE_URL");
-  private static final String REALM = System.getenv("KEYCLOAK_REALM");
-  private static final String CLIENT_ID = System.getenv("KEYCLOAK_CLIENT_ID");
-  private static final String CLIENT_SECRET = System.getenv("KEYCLOAK_CLIENT_SECRET");
+    private static final String BASE_URL = System.getenv("KEYCLOAK_BASE_URL");
+    private static final String REALM = System.getenv("KEYCLOAK_REALM");
+    private static final String CLIENT_ID = System.getenv("KEYCLOAK_CLIENT_ID");
+    private static final String CLIENT_SECRET = System.getenv("KEYCLOAK_CLIENT_SECRET");
 
     @Override
     public void start(Promise<Void> startPromise) throws Exception 
@@ -37,61 +37,56 @@ public class KeycloakDiscoverVerticle extends AbstractVerticle
         // Create the HTTP server
         HttpServerOptions options = new HttpServerOptions();
         vertx.createHttpServer(options)
-            // Handle every request using the router
-            .requestHandler(router)
-            // Start listening
-            .listen(bindPort, bindAddress)
-            // Print the port
-            .onSuccess(server -> {
-                startPromise.complete();
-                LOG.log(Level.INFO, String.format("HTTP server started on port %s", server.actualPort()));
-            })
-            .onFailure(event -> {
-                startPromise.fail(event);
-                LOG.log(Level.SEVERE, String.format("Failed to start HTTP server:%s", event.getMessage()));
-            }
-        );
+                // Handle every request using the router
+                .requestHandler(router)
+                // Start listening
+                .listen(bindPort, bindAddress)
+                // Print the port
+                .onSuccess(server -> {
+                    startPromise.complete();
+                    LOG.log(Level.INFO,
+                            String.format("HTTP server started on port %s", server.actualPort()));
+                }).onFailure(event -> {
+                    startPromise.fail(event);
+                    LOG.log(Level.SEVERE,
+                            String.format("Failed to start HTTP server:%s", event.getMessage()));
+                });
 
     }
 
     private Router initRoutes()
     {
         // set up keycloak authentication
-        final Future<OAuth2Auth> oa2 = OpenIDConnectAuth.discover(
-                vertx,
-                new OAuth2Options()
-                        .setClientId(CLIENT_ID)
-                        .setTenant(REALM)
-                        .setClientSecret(CLIENT_SECRET)
-                        .setSite(String.format("%s/realms/%s", BASE_URL, REALM)))
+        final Future<OAuth2Auth> oa2 = OpenIDConnectAuth
+                .discover(vertx,
+                        new OAuth2Options().setClientId(CLIENT_ID).setTenant(REALM)
+                                .setClientSecret(CLIENT_SECRET)
+                                .setSite(String.format("%s/realms/%s", BASE_URL, REALM)))
 
-                .onSuccess(oauth2 ->
-                {
+                .onSuccess(oauth2 -> {
                     // the setup call succeeded.
                     // at this moment your auth is ready to use
                     LOG.log(Level.INFO, "OAuth2 setup call succeeded!!!");
-                })
-                .onFailure(err -> {
+                }).onFailure(err -> {
                     // the setup failed.
-                    LOG.warning(String.format("Initialization of OAuth2 failed: %s", err.getMessage()));
+                    LOG.warning(
+                            String.format("Initialization of OAuth2 failed: %s", err.getMessage()));
                 });
 
         Router router = Router.router(vertx);
 
         // health check
-        router.get("/")
-            .handler(ctx -> {
-                ctx.response()
-                .end("howdy!");
-            });
+        router.get("/").handler(ctx -> {
+            ctx.response().end("howdy!");
+        });
 
         return router;
     }
 
     private void readConfigProps() throws IllegalArgumentException, InterruptedException
     {
-      bindAddress = "0.0.0.0";
-      bindPort = 8081;
+        bindAddress = "0.0.0.0";
+        bindPort = 8081;
     }
-  
+
 }
--- src/main/java/com/lumen/vertx_oauth2_oidc/ProtectedProfileVerticle.java
+++ src/main/java/com/lumen/vertx_oauth2_oidc/ProtectedProfileVerticle.java
@@ -10,59 +10,50 @@ import io.vertx.ext.web.templ.handlebars.HandlebarsTemplateEngine;
 
 public class ProtectedProfileVerticle extends AbstractVerticle {
 
-  private String bindAddress;
-  private int bindPort;
+    private String bindAddress;
+    private int bindPort;
 
-  private static final String CLIENT_ID = System.getenv("GITHUB_CLIENT_ID");
-  private static final String CLIENT_SECRET = System.getenv("GITHUB_CLIENT_SECRET");
+    private static final String CLIENT_ID = System.getenv("GITHUB_CLIENT_ID");
+    private static final String CLIENT_SECRET = System.getenv("GITHUB_CLIENT_SECRET");
 
-  @Override
-  public void start(Promise<Void> startPromise) throws Exception {
-    readConfigProps();
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception {
+        readConfigProps();
 
-    HandlebarsTemplateEngine engine =
-      HandlebarsTemplateEngine.create(vertx);
+        HandlebarsTemplateEngine engine = HandlebarsTemplateEngine.create(vertx);
 
-    Router router = Router.router(vertx);
+        Router router = Router.router(vertx);
 
-    router.get("/")
-      .handler(ctx -> {
-        // we pass the client id to the template
-        ctx.put("client_id", CLIENT_ID);
-        // and now delegate to the engine to render it.
-        engine.render(ctx.data(), "views/index.hbs")
-          .onSuccess(buffer -> {
-            ctx.response()
-              .putHeader("Content-Type", "text/html")
-              .end(buffer);
-          })
-          .onFailure(ctx::fail);
-      });
+        router.get("/").handler(ctx -> {
+            // we pass the client id to the template
+            ctx.put("client_id", CLIENT_ID);
+            // and now delegate to the engine to render it.
+            engine.render(ctx.data(), "views/index.hbs").onSuccess(buffer -> {
+                ctx.response().putHeader("Content-Type", "text/html").end(buffer);
+            }).onFailure(ctx::fail);
+        });
 
-    OAuth2Auth authProvider = GithubAuth.create(vertx, CLIENT_ID, CLIENT_SECRET);
+        OAuth2Auth authProvider = GithubAuth.create(vertx, CLIENT_ID, CLIENT_SECRET);
 
-    router.get("/protected")
-      .handler(
-        // OAuth2AuthHandler.create(vertx, authProvider, "http://localhost:8080/callback")
-        OAuth2AuthHandler.create(vertx, authProvider, "https://vertx.local/callback")
-          .setupCallback(router.route("/callback"))
-          .withScope("user:email"))
-      .handler(new ProtectedProfileHandler(authProvider, engine));
+        router.get("/protected").handler(
+                // OAuth2AuthHandler.create(vertx, authProvider, "http://localhost:8080/callback")
+                OAuth2AuthHandler
+                        .create(vertx, authProvider, "https://vertx.local/callback")
+                        .setupCallback(router.route("/callback"))
+                        .withScope("user:email"))
+                .handler(new ProtectedProfileHandler(authProvider, engine));
 
-    vertx.createHttpServer()
-      .requestHandler(router)
-      .listen(bindPort, bindAddress)
-      .onSuccess(server -> {
-        System.out.println(
-          "HTTP server started on port: " + server.actualPort());
-        startPromise.complete();
-      }).onFailure(startPromise::fail);
-  }
+        vertx.createHttpServer().requestHandler(router).listen(bindPort, bindAddress)
+                .onSuccess(server -> {
+                    System.out.println(
+                            "HTTP server started on port: " + server.actualPort());
+                    startPromise.complete();
+                }).onFailure(startPromise::fail);
+    }
 
-  private void readConfigProps() throws IllegalArgumentException, InterruptedException
-  {
-    bindAddress = "0.0.0.0";
-    bindPort = 8080;
-  }
+    private void readConfigProps() throws IllegalArgumentException, InterruptedException {
+        bindAddress = "0.0.0.0";
+        bindPort = 8080;
+    }
 
 }
